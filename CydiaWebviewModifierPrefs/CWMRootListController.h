324a7e0aa69f426abc8d0656d14cb9fb234b2423
LukeeGD <26163116+LukeZGD@users.noreply.github.com>
2026-04-03 19:43:08 +0800

    test

--- CydiaWebviewModifierPrefs/CWMRootListController.h
+++
@@ -1,5 +0,0 @@
-#import <Preferences/PSListController.h>
-
-@interface CWMRootListController : PSListController
-
-@end
--- README.md
+++
@@ -1,3 +0,0 @@
-# Cydia Webview Modifier
-
-Tweak to modify URL loaded in Cydia webviews
